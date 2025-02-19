23c72d194d422e63ebc7237e218bbdb4355d9f72
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2025-02-19 19:54:34 -0300

    :tada: Commit inicial

---
+++ src/main/java/br/com/fiap/odontoprevjavamvc/OdontoprevJavaMvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.odontoprevjavamvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontoprevJavaMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontoprevJavaMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/odontoprevjavamvc/OdontoprevJavaMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.odontoprevjavamvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontoprevJavaMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
