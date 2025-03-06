e5e378caf74c70f2ae09fda914e0ff5f28dd96ee
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2025-03-06 18:06:11 -0300

    :sparkles: feat: GET e POST PacienteController

--- src/main/java/br/com/fiap/odontoprevjavamvc/controller/PacienteController.java
+++ src/main/java/br/com/fiap/odontoprevjavamvc/controller/PacienteController.java
@@ -1,10 +1,15 @@
 package br.com.fiap.odontoprevjavamvc.controller;
 
+import br.com.fiap.odontoprevjavamvc.dto.PacienteRequest;
 import br.com.fiap.odontoprevjavamvc.model.Paciente;
 import br.com.fiap.odontoprevjavamvc.service.PacienteService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,5 +26,27 @@ public class PacienteController {
         List<Paciente> pacientes = pacienteService.buscarPacientes();
         mv.addObject("listaPacientes", pacientes);
         return mv;
+    }
+    @GetMapping("/cadastro")
+    public ModelAndView pacienteCadastro(){
+        ModelAndView mv = new ModelAndView("pacienteCadastro");
+        mv.addObject("paciente",new Paciente());
+        return mv;
+    }
+    @PostMapping("/cadastrar")
+    public ModelAndView cadastrarPaciente(@Valid @ModelAttribute("Paciente") PacienteRequest pacienteRequest, BindingResult result) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        if (result.hasErrors()) {
+            modelAndView.setViewName("pacienteCadastro"); // Retorna a página com erros
+            modelAndView.addObject("paciente",new Paciente());
+            modelAndView.addObject("errors",result.getAllErrors());
+            return modelAndView;
+        }
+
+        pacienteService.cadastrarPaciente(pacienteRequest);
+        modelAndView.setViewName("redirect:/paciente/lista"); // Redireciona para a página de sucesso
+
+        return modelAndView;
     }
 }
