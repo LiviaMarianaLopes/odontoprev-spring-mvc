f3c453ccde006a4c91499cb3ef8867b20bcac4d6
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2025-03-07 17:07:39 -0300

    :sparkles: feat: atualizar e deletar paciente no controller

--- src/main/java/br/com/fiap/odontoprevjavamvc/controller/PacienteController.java
+++ src/main/java/br/com/fiap/odontoprevjavamvc/controller/PacienteController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -48,5 +45,38 @@ public class PacienteController {
         modelAndView.setViewName("redirect:/paciente/lista"); // Redireciona para a página de sucesso
 
         return modelAndView;
+    }
+    @GetMapping("/edicao/{id}")
+    public ModelAndView pacienteEdicao(@PathVariable Long id){
+        Paciente paciente = pacienteService.buscarPaciente(id);
+        if(paciente == null){
+            return listarPacientes();
+        }
+        ModelAndView mv = new ModelAndView("pacienteEdicao");
+        mv.addObject("idPaciente", id);
+        mv.addObject("paciente", pacienteService.pacienteToRequest(paciente));
+        System.out.println(pacienteService.pacienteToRequest(paciente));
+        return mv;
+    }
+    @PostMapping("/editar/{id}")
+    public ModelAndView editarPaciente(@PathVariable Long id, @Valid @ModelAttribute PacienteRequest pacienteRequest, BindingResult result){
+        ModelAndView modelAndView = new ModelAndView();
+
+        if (result.hasErrors()) {
+            modelAndView.setViewName("pacienteCadastro"); // Retorna a página com erros
+            modelAndView.addObject("paciente",new Paciente());
+            modelAndView.addObject("errors",result.getAllErrors());
+            return modelAndView;
+        }
+        pacienteService.atualizarPaciente(id, pacienteRequest);
+        modelAndView.setViewName("redirect:/paciente/lista"); // Redireciona para a página de sucesso
+
+        return modelAndView;
+    }
+
+    @GetMapping("/deletar/{id}")
+    public ModelAndView deletarPaciente(@PathVariable Long id){
+        pacienteService.deletarPaciente(id);
+        return listarPacientes();
     }
 }
